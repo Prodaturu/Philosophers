d953f11308fa2fd6fb10d14a12f3abf6411c8995
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-06-07 21:42:27 +0200

    Removed executable from git and added timestamps to the prints

--- routine.c
+++ routine.c
@@ -6,11 +6,19 @@
 /*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 20:50:08 by sprodatu          #+#    #+#             */
-/*   Updated: 2024/06/07 20:53:07 by sprodatu         ###   ########.fr       */
+/*   Updated: 2024/06/07 21:40:33 by sprodatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+void	print_action(t_philo *philo, const char *action)
+{
+	size_t	timestamp;
+
+	timestamp = get_curr_time() - philo->shared->start_time;
+	printf("%zu %d %s\n", timestamp, philo->id, action);
+}
 
 void	*philo_routine(void *arg)
 {
@@ -19,21 +27,36 @@ void	*philo_routine(void *arg)
 	philo = (t_philo *)arg;
 	while (!philo->shared->is_dead)
 	{
-		printf("Philosopher %d is thinking.\n", philo->id);
+		print_action(philo, "is thinking");
 		pthread_mutex_lock(&philo->r_fork);
+		print_action(philo, "has taken a fork");
 		pthread_mutex_lock(philo->l_fork);
-		printf("Philosopher %d picked up forks.\n", philo->id);
+		print_action(philo, "has taken a fork");
 		pthread_mutex_lock(&philo->lock_eating);
 		philo->last_meal_time = get_curr_time();
 		philo->meal_done_count++;
-		printf("Philosopher %d is eating.\n", philo->id);
+		print_action(philo, "is eating");
 		pthread_mutex_unlock(&philo->lock_eating);
 		usleep(philo->shared->time_to_eat * 1000);
 		pthread_mutex_unlock(philo->l_fork);
 		pthread_mutex_unlock(&philo->r_fork);
-		printf("Philosopher %d put down forks.\n", philo->id);
-		printf("Philosopher %d is sleeping.\n", philo->id);
+		print_action(philo, "is sleeping");
 		usleep(philo->shared->time_to_sleep * 1000);
 	}
 	return (NULL);
 }
+// Explanation
+// Timestamp Calculation:
+
+// The get_curr_time function returns the current time in milliseconds.
+// The print_action function calculates the timestamp
+// by subtracting philo->shared->start_time from the current time.
+// Print Statements:
+
+// The print_action function is used to print actions with the correct format.
+// It prints the timestamp, philosopher ID, and action.
+// Action Descriptions:
+
+// Each action (thinking, taking a fork, eating, and sleeping)
+// is logged using the print_action function,
+// ensuring the output is consistent with the required format.
